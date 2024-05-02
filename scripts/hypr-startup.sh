e1720919d308b7afe15709750a827e1e2b1a76ca
firesquid6 <jdeiss06@gmail.com>
2024-05-01 20:24:20 -0500

    remove unused scripts

--- scripts/hypr-startup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-swww init &
-swww img ~/wallpapers/wallpaper.png &
-
-/etc/nixos/scripts/waybar.sh &
-
--- scripts/i3-startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-setxkbmap -option caps:escape
-feh --bg-fill /home/firesquid/wallpapers/wallpaper.png
